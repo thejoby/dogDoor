65110077333f42403155729f3e18067302586a84
Joe Burch <thejoby@gmail.com>
2020-02-15 11:23:40 -0800

    Switching from RPIO to RPi.GPIO

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 #from Tkinter import *
 import time
-from RPIO import PWM
+#from RPIO import PWM
+import RPi.GPIO as GPIO
 import sys
 
 
@@ -20,18 +21,37 @@ def cakes():
 
 @app.route('/open')
 def open():
-    servo = PWM.Servo()
-    servo.set_servo(18, 1800)
-    time.sleep(2)
-    servo.stop_servo(18)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(18, GPIO.OUT)
+    p = GPIO.PWM(18, 50)
+    p.start(4)
 
+    #servo = PWM.Servo()
+    #servo.set_servo(18, 1800)
+    time.sleep(2)
+    #servo.stop_servo(18)
+    p.stop()
+    GPIO.cleanup()
 
 @app.route('/close')
 def close():
-    servo = PWM.Servo()
-    servo.set_servo(18, 810)
-    time.sleep(1.5)
-    servo.stop_servo(18)
+    #servo = PWM.Servo()
+    #servo.set_servo(18, 810)
+    #time.sleep(1.5)
+    #servo.stop_servo(18)
+
+    #https://rpi.science.uoit.ca/lab/servo/
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(18, GPIO.OUT)
+    #GPIO.PWM(PIN, frequency in Hz)
+    p = GPIO.PWM(18, 50)
+    #p.start(duty_cycle)
+    #The duty cycle describes the proportion of on time 
+    # to the regular interval or period of time.
+    p.start(10)
+    time.sleep(2)
+    p.stop()
+    GPIO.cleanup()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
