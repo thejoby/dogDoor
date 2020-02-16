5e237bcbcd808f18d90ab49f92c3e58604540d7e
Joe Burch <thejoby@gmail.com>
2020-02-15 19:58:12 -0800

    Add message for GET requests

--- app.py
+++ app.py
@@ -14,35 +14,41 @@ def index():
     return 'Flask app is running at: %s' % (now)
 
 
-@app.route('/open', methods=['POST'])
+@app.route('/open', methods=['GET','POST'])
 def open():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    p = GPIO.PWM(18, 100)
-    p.start(17.5)
+    if request.method == 'GET':
+        return "POST to this endpoint instead"
+    if request.method == 'POST':
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
+        p = GPIO.PWM(18, 100)
+        p.start(17.5)
 
-    time.sleep(1.2)
+        time.sleep(1.2)
 
-    p.stop()
-    GPIO.cleanup()
-    return 'Dog Door Open!'
+        p.stop()
+        GPIO.cleanup()
+        return 'Dog Door Open!'`
 
 
-@app.route('/close', methods=['POST'])
+@app.route('/close', methods=['GET','POST'])
 def close():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    #GPIO.PWM(PIN, frequency in Hz)
-    p = GPIO.PWM(18, 100)
-    #p.start(duty_cycle)
-    #The duty cycle describes the proportion of on time 
-    # to the regular interval or period of time.
-    p.start(7)
-    
-    time.sleep(1)
-    p.stop()
-    GPIO.cleanup()
-    return 'Dog Door Closed!'
+    if request.method == 'GET':
+        return "POST to this endpoint instead"
+    if request.method == 'POST':
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
+        #GPIO.PWM(PIN, frequency in Hz)
+        p = GPIO.PWM(18, 100)
+        #p.start(duty_cycle)
+        #The duty cycle describes the proportion of on time 
+        # to the regular interval or period of time.
+        p.start(7)
+        
+        time.sleep(1)
+        p.stop()
+        GPIO.cleanup()
+        return 'Dog Door Closed!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
